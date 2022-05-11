8c7aede54e775a142e1fd03b3c18a0666b3fbd7c
Duy Nguyễn <duynguyen1302.bm@gmail.com>
2022-05-11 22:50:48 +0700

    Update audit.sql

--- ScriptDB/database/Audit/Standard Audit/audit.sql
+++ ScriptDB/database/Audit/Standard Audit/audit.sql
@@ -1,20 +1,20 @@
 alter system set audit_trail='db','extended' scope=spfile;
 
---•	Audit vi?c thêm, xóa, c?p nh?t d? li?u b?ng CSYT c?a DBA 
+--ï¿½	Audit vi?c thï¿½m, xï¿½a, c?p nh?t d? li?u b?ng CSYT c?a DBA 
 audit insert, delete, update on QTV.CSYT by access;
 
 --noaudit all on qtv.CSYT;
 
---•	Audit vi?c thêm xóa, c?p nh?ta d? li?u cho b?ng nhân viên 
+--ï¿½	Audit vi?c thï¿½m xï¿½a, c?p nh?ta d? li?u cho b?ng nhï¿½n viï¿½n 
 audit insert,delete, update on QTV.NHANVIEN by access;
 --noaudit all on qtv.NHANVIEN;
 
---•	Audit vi?c th?c thi procedure themHSBA 
+--ï¿½	Audit vi?c th?c thi procedure themHSBA 
 AUDIT EXECUTE ON THEMHSBA BY ACCESS;
 
 --noaudit all on qtv.THEMHSBA;
 
---•	Audit vi?c xem, thêm, xóa, s?a c?a b?ng khóa ng??i dùng UK
+--ï¿½	Audit vi?c xem, thï¿½m, xï¿½a, s?a c?a b?ng khï¿½a ng??i dï¿½ng UK
 AUDIT INSERT,UPDATE,DELETE ON QTV.UKNHANVIEN;
 
 --noaudit all on qtv.UKNHANVIEN;
@@ -22,17 +22,5 @@ AUDIT INSERT,UPDATE,DELETE ON QTV.UKNHANVIEN;
 AUDIT INSERT,UPDATE,DELETE ON QTV.UKHSBA;
 --noaudit all on qtv.UKHSBA;
 
-select USERNAME,OBJ_NAME,SQL_TEXT ,EXTENDED_TIMESTAMP
-from dba_audit_trail order by extended_timestamp;
-
-
-TRUNCATE TABLE SYS.AUD$;
-
-
-alter system set audit_trail='db','extended' scope=spfile;
-
-
-UPDATE QTV.NHANVIEN SET VAITRO='THANH TRA'
-SELECT * FROM QTV.NHANVIEN;
 COMMIT;
 
